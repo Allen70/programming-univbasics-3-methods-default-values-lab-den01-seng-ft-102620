1515d2048434e41ef358a01e84733f5f37fd89db
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-03 10:35:57 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,8 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice 
+def meal_choice protein
   
   puts "What a nutritious meal!"
+  puts "#{protein}"
 end
